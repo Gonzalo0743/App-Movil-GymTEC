72e2d3695ca3a0ea950dd6be2407cda0a7fc34d2
Gonzalo0743 <gonzaloa0743@gmail.com>
2023-05-01 19:19:17 -0600

    Adding the login/register interface

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,18 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.myapplication.db.DbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
+        Log.d(TAG, "onCreate: Starting");
+
+        Button RegisterButton = (Button) findViewById(R.id.RegisterButton);
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Clicked RegisterButton");
+
+                Intent intent = new Intent(MainActivity.this, RegisterLayout.class);
+                startActivity(intent);
+            }
+        });
 
         DbHelper dbHelper = new DbHelper(MainActivity.this);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
---
+++ MyApplication/app/src/main/java/com/example/myapplication/RegisterLayout.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterLayout extends AppCompatActivity {
+
+    private static final String TAG = "RegisterLayout";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+
+        Log.d(TAG, "onCreate: Starting");
+
+        Button BackButton = (Button) findViewById(R.id.BackButton);
+
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Clicked BackButton");
+
+                Intent intent = new Intent(RegisterLayout.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
